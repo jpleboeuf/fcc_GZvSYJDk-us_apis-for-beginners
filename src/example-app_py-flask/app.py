760de4769391c94409a3c97082d08ee50784b8f4
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2022-07-28 15:27:03 +0200

    Python example app.: complete the app.
    Completed the Python example app.
     with a small modification
     to filter out messages non-related to the example app.

--- src/example-app_py-flask/app.py
+++ src/example-app_py-flask/app.py
@@ -9,6 +9,7 @@ from flask import (
     request,
     url_for,
 )
+from twilio.rest import Client
 
 load_dotenv()
 app = Flask(__name__)
@@ -16,14 +17,17 @@ app.secret_key = "ssssh don't tell anyone"
 
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
 
+client = Client()
+
 def get_sent_messages():
-    # TODO: Make this return a collection of messages that were sent from the number
-    messages = []
+    """returns a collection of messages that were sent from the number"""
+    messages = client.messages.list(from_=TWILIO_PHONE_NUMBER)
+    messages = filter(lambda messages: "See more compliments at " in messages.body, messages)
     return messages
 
 def send_message(to, body):
-    # TODO: Send the text message
-    pass
+    """sends the text message"""
+    new_msg = client.messages.create(from_=TWILIO_PHONE_NUMBER, to=to, body=body)
 
 @app.route("/", methods=["GET"])
 def index():
